644769b18ba842fcd7f66bf3293a2469531e8f61
RoksanaStanczyk <Rstanczyk24@gmail.com>
2020-05-05 15:39:55 +0200

    image classification cnn matlab

--- cnn.m
+++ cnn.m
@@ -1,60 +1,46 @@
-a= imread('1.png'); %zdj poddane klasyfikacji
-figure, imshow(a)
+% zdjęcia w zakresie szarości 64x64
+% wykorzystane wszytkie zdjęcia z bazy: 
+%449- zmiany łagodne, 208 - zmiany złośliwe
+
 outputFolder=fullfile('Baza');
 Datasetpath= fullfile(outputFolder, 'Guzy');
 Data = imageDatastore(Datasetpath, 'IncludeSubfolders', true, 'LabelSource', 'foldernames')
 
-
-tbl = countEachLabel(Data); %%liczy ile jest zdjęć w danej kategorii, mogą być różne wartości czego nie chcemy
-minSetCount = min(tbl{:,2}) %% druga kolumna
+tbl = countEachLabel(Data);
+minSetCount = min(tbl{:,2})
 % same count of images
 Data=splitEachLabel(Data, minSetCount, 'randomize');
 countEachLabel(Data);
 
 imageAugmenter = imageDataAugmenter( ...
-    'RandRotation',[-10 10], ...
-    'RandXTranslation',[-3 3], ...
-    'RandYTranslation',[-3 3])
+    'RandRotation',[-40 40], ...
+    'RandXTranslation',[-8 8], ...
+    'RandYTranslation',[-8 8])
 
-
-imageSize=[64 64 1];
-
-%  numTrainFiles = 79; 
-[DataTrain,DataValidation,DataTest] = splitEachLabel(Data,0.7,0.15, 'randomize')
+[DataTrain,DataValidation,DataTest] = splitEachLabel(Data,0.7,0.15, 'randomize')% 70%-trening, 15%-validacja, 15%-test
 % [DataTrain, DataValidation] = splitEachLabel(Data, 0.3, 'randomize')
 Traindatasource = augmentedImageSource([64 64 1],DataTrain,'DataAugmentation',imageAugmenter);
 
 minibatch = preview(Traindatasource);
 imshow(imtile(minibatch.input));
 
-% figure;
-% perm = randperm(100, 20);
-% for i = 1:20
-%     subplot(5,4,i);
-%     imshow(Data.Files{perm(i)});
-% end
-% img = readimage(Data,1);
-% size(img)
-
-
-
-
 
 layers = [ ...
     imageInputLayer([64 64 1])
-    convolution2dLayer(6, 8, 'Padding', 'same' ) 
+    convolution2dLayer(3, 8, 'Padding', 'same' ) 
     batchNormalizationLayer 
-    reluLayer %f.aktywacji
+%   clippedReluLayer(10)
+    reluLayer
     
     maxPooling2dLayer(2,'stride',2) 
     
-    convolution2dLayer(6, 6 ,'Padding', 'same')
+    convolution2dLayer(3, 16,'Padding', 'same' )
     batchNormalizationLayer 
     reluLayer
     
     maxPooling2dLayer(2,'stride',2)
     
-    convolution2dLayer(6, 4 ,'Padding', 'same')
+    convolution2dLayer(3, 32 ,'Padding', 'same')
     batchNormalizationLayer 
     reluLayer
     
@@ -65,28 +51,30 @@ layers = [ ...
     classificationLayer]
 
 
-miniBatchSize = 64
+miniBatchSize = 256
 % validationFrequency = floor(numel(DataValidation)/miniBatchSize);
-options=trainingOptions('adam', 'MaxEpochs', 20 ,'MiniBatchSize', miniBatchSize,...
+options=trainingOptions('adam', 'MaxEpochs', 25 ,'MiniBatchSize', miniBatchSize,...
     'initialLearnRate',0.001, 'Shuffle', 'every-epoch', ...
     'ValidationData', DataValidation, 'ValidationFrequency', 2, ...
     'Verbose', 1,  'Plots', 'training-progress',...
     'OutputFcn',@(info)stopIfAccuracyNotImproving(info,3));
 
-%  'ValidationPatience',5,
-% 
 % convnet = trainNetwork(DataTrain, layers, options)
 convnet = trainNetwork(Traindatasource, layers, options)
 
-% YPred = classify(convnet,DataValidation);
-% YValidation = DataValidation.Labels;
-% accuracy = sum(YPred == YValidation)/numel(YValidation)
-% output = classify(convnet, a);
-
+%% Measure network accuracy
+load convnet
 predLabelsTest = classify(convnet, DataTest);
 labelsTest = DataTest.Labels;
 accuracy = sum(predLabelsTest == labelsTest) / numel(labelsTest)
-% output = classify(predLabelsTest, a); 
+analyzeNetwork(convnet)
+%% after train
+load convnet 
+a= imread('3.png'); %zdj poddane klasyfikacji
+figure, imshow(a)
+net = convnet; 
+output = classify(net, a)
+
 
  tf1=[]
 for  ii=1:2

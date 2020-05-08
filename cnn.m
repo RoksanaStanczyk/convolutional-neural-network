17894bc09b439a8a876be0e1a17e75bb2f53b4f6
RoksanaStanczyk <Rstanczyk24@gmail.com>
2020-05-08 12:22:55 +0200

    -

--- cnn.m
+++ cnn.m
@@ -17,9 +17,19 @@ imageAugmenter = imageDataAugmenter( ...
     'RandXTranslation',[-8 8], ...
     'RandYTranslation',[-8 8])
 
-[DataTrain,DataValidation,DataTest] = splitEachLabel(Data,0.7,0.15, 'randomize')% 70%-trening, 15%-validacja, 15%-test
-% [DataTrain, DataValidation] = splitEachLabel(Data, 0.3, 'randomize')
-Traindatasource = augmentedImageSource([64 64 1],DataTrain,'DataAugmentation',imageAugmenter);
+[DataTrain,DataValidation,DataTest] = splitEachLabel(Data,0.5,0.2, 0.3)
+inputImageSize = [64 64 1]; 
+dataTrain = augmentedImageDatastore(inputImageSize, DataTrain,...
+                                    'ColorPreprocessing',       'rgb2gray',...
+                                    'DataAugmentation', imageAugmenter);
+dataValidation = augmentedImageDatastore(inputImageSize, DataValidation,...
+                                    'ColorPreprocessing',       'rgb2gray',...
+                                    'DataAugmentation', imageAugmenter);
+dataTest = augmentedImageDatastore(inputImageSize, DataTest,...
+                                    'ColorPreprocessing',       'rgb2gray',...
+                                    'DataAugmentation', imageAugmenter);
+
+% Traindatasource = augmentedImageSource([64 64 1],DataTrain,'DataAugmentation',imageAugmenter);
 
 minibatch = preview(Traindatasource);
 imshow(imtile(minibatch.input));
@@ -32,45 +42,46 @@ layers = [ ...
 %   clippedReluLayer(10)
     reluLayer
     
-    maxPooling2dLayer(2,'stride',2) 
+    maxPooling2dLayer(3,'stride',3) 
     
     convolution2dLayer(3, 16,'Padding', 'same' )
     batchNormalizationLayer 
     reluLayer
     
-    maxPooling2dLayer(2,'stride',2)
+    maxPooling2dLayer(3,'stride',3)
     
-    convolution2dLayer(3, 32 ,'Padding', 'same')
+    convolution2dLayer(5, 32 ,'Padding', 'same')
     batchNormalizationLayer 
     reluLayer
     
-    maxPooling2dLayer(2,'stride',2)
+    maxPooling2dLayer(5,'stride',5)
     
     fullyConnectedLayer(2)
     softmaxLayer
     classificationLayer]
 
 
-miniBatchSize = 256
+miniBatchSize = 128
 % validationFrequency = floor(numel(DataValidation)/miniBatchSize);
-options=trainingOptions('adam', 'MaxEpochs', 25 ,'MiniBatchSize', miniBatchSize,...
-    'initialLearnRate',0.001, 'Shuffle', 'every-epoch', ...
-    'ValidationData', DataValidation, 'ValidationFrequency', 2, ...
+options=trainingOptions('adam', 'MaxEpochs', 28 ,'MiniBatchSize', miniBatchSize,...
+    'Shuffle', 'every-epoch', ...
+    'ValidationData', dataValidation, 'ValidationFrequency', 2, ...
     'Verbose', 1,  'Plots', 'training-progress',...
     'OutputFcn',@(info)stopIfAccuracyNotImproving(info,3));
 
+%     'initialLearnRate',0.001,
 % convnet = trainNetwork(DataTrain, layers, options)
-convnet = trainNetwork(Traindatasource, layers, options)
-
+convnet = trainNetwork(dataTrain, layers, options)
+save convnet 
 %% Measure network accuracy
 load convnet
-predLabelsTest = classify(convnet, DataTest);
+predLabelsTest = classify(convnet, dataTest);
 labelsTest = DataTest.Labels;
 accuracy = sum(predLabelsTest == labelsTest) / numel(labelsTest)
 analyzeNetwork(convnet)
 %% after train
 load convnet 
-a= imread('3.png'); %zdj poddane klasyfikacji
+a= imread('6.png'); %zdj poddane klasyfikacji
 figure, imshow(a)
 net = convnet; 
 output = classify(net, a)
